f4a735b98b7a0482df39ce495dbf163429963cf2
Gal Sandor <galsanyi84@gmail.com>
2026-09-15 00:18:35 +0200

    Initialize Next.js app and add header navigation

---
+++ app/src/app/page.js
@@ -0,0 +1,18 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <>
+      <header>
+        <Link className='text-red-500 font-semibold text-2xl' href=''>SANYI Pizza</Link>
+        <nav>
+          <Link href={''}>Home</Link>
+          <Link href={''}>Menu</Link>
+          <Link href={''}>About</Link>
+          <Link href={''}>Contact</Link>
+          <Link href={''}>Login</Link>
+        </nav>
+      </header>
+    </>
+  );
+}
